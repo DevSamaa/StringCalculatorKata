58c82da30ef9e62a8cc9d6ce07207948862a2e48
DevSamaa <samaakanani@gmail.com>
2020-05-26 11:25:39 +1000

    initial commit

---
+++ StringCalculatorKata/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StringCalculatorKata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+// Create a simple string calculator with a method that takes a string and returns a number.
+//
+// Add("") > Returns 0
