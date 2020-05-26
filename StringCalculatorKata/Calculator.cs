df7600af9caf09460a09dc5e1855f4e1e2326eb4
DevSamaa <samaakanani@gmail.com>
2020-05-26 16:21:24 +1000

    deleted unnecessary code from Add method

--- StringCalculatorKata/Calculator.cs
+++ StringCalculatorKata/Calculator.cs
@@ -8,41 +8,6 @@ namespace StringCalculatorKata
 
         public int Add(string incomingString)
         {
-            // var isNumber = int.TryParse(incomingString, out int result);
-            // return isNumber ? result : 0;
-            
-            // var number1 = Convert.ToInt32(incomingString[0]);
-            // var number2 = Convert.ToInt32(incomingString[2]);
-            // return number1 + number2;
-            
-            // var listOfNumbers =incomingString.Where(c => c >= 0);
-            // listOfNumbers.Sum()
-            // return listOfNumbers;
-            
-            // int result = 0;
-            // foreach (var c in incomingString)
-            // {
-            //     if (c >= 0 && c !=',' )
-            //     {
-            //         result += int.Parse(c.ToString());
-            //     }
-            // }
-            //
-            // return result;
-
-            // var numbers = incomingString.Split(",");
-            // var result = 0;
-            // foreach (var number in numbers)
-            // {
-            //     var isNumber = int.TryParse(number, out int resultOfParse);
-            //     if (!isNumber)
-            //     {
-            //         break;
-            //     }
-            //     result += resultOfParse;
-            // }
-            // return result;
-            
             if (string.IsNullOrEmpty(incomingString))
             {
                 return 0;
@@ -50,19 +15,6 @@ namespace StringCalculatorKata
             var numbers = incomingString.Split(",");
             return numbers.Sum(number => int.Parse(number));
             
-            // int sum = 0;
-            // foreach(char c in incomingString)
-            // {
-            //     if(c == 44)
-            //     {
-            //         continue;
-            //     }
-	           //
-            //     sum += c - 48;
-            // }
-            //
-            // return sum;
-
         }
         
     }
